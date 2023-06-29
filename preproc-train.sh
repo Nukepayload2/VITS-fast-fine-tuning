693b47578e2074672804766720f4e639cb65d9cc
Nukepayload2 <nukepayload2@outlook.com>
2023-06-29 09:18:06 +0800

    Add publish artifacts

--- preproc-train.sh
+++ preproc-train.sh
@@ -18,3 +18,10 @@ python3 finetune_speaker_v2.py -m ./OUTPUT_MODEL --max_epochs "100" --drop_speak
 
 # Continue training (comment if you need to start new training)
 #python3 finetune_speaker_v2.py -m ./OUTPUT_MODEL --max_epochs "100" --drop_speaker_embed False --cont True
+
+# Publish artifacts
+python3 rearrange_speaker.py
+mkdir OUTPUT_MODEL
+cp finetune_speaker.json OUTPUT_MODEL
+cp G_latest.pth OUTPUT_MODEL
+cp moegoe_config.json OUTPUT_MODEL
